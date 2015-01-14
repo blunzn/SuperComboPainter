08f79a915359d32ba9655d4bc339a28a14ea1ad3
Peter Vorlaufer <peter@brokenrul.es>
2015-01-14 18:30:01 +0100

    added posturekeeper

---
+++ ComboPainter/Assets/Resources/Prefabs/PostureKeeper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostureKeeper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HingeJoint2D j = gameObject.GetComponent<HingeJoint2D> ();
+		float deltaAngle = j.jointAngle;
+		JointMotor2D motor = j.motor;
+		motor.motorSpeed = deltaAngle;
+	}
+
+}
