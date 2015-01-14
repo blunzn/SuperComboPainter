6223e40468307ac89c5576ff6808d61e1ac71900
Jan Hackl <jan@brokenrul.es>
2015-01-14 21:19:06 +0100

    hands are painting now (with the Brush component)

---
+++ ComboPainter/Assets/Brush.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brush : MonoBehaviour {
+
+	private PaintArea canvas;
+
+	// Use this for initialization
+	void Start () {
+
+		canvas = GameObject.Find("Canvas").GetComponent<PaintArea>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		canvas.paintAt(transform.position);
+	}
+}
