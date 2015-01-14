8ab9a4d32e2e6c44ad698f60c7845861c24c32ec
Jan Hackl <jan@brokenrul.es>
2015-01-14 19:34:22 +0100

    cleaned up a bit

--- ComboPainter/Assets/Canvas.cs
+++ ComboPainter/Assets/Canvas.cs
@@ -5,17 +5,19 @@ public class Canvas : MonoBehaviour {
 
 	public Vector3 position;
 
+	private Texture2D texture;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		texture = GetComponent<SpriteRenderer>().sprite.texture;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
 		position = Input.mousePosition;
-		sprite.sprite.texture.SetPixel((int)position.x, (int)position.y, Color.red);
-		sprite.sprite.texture.Apply();
+		texture.SetPixel((int)position.x, (int)position.y, Color.red);
+		texture.Apply();
 	}
 }
