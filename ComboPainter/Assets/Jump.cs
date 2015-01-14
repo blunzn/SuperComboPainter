1fae5a1ea2f38e90904acff30ab8db66d5302497
Felix Bohatsch <felix@brokenrul.es>
2015-01-14 17:35:21 +0100

    initial commit

---
+++ ComboPainter/Assets/Jump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	public float jumpForce = 2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+						rigidbody2D.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+	}
+}
---
+++ ComboPainter/Assets/Walk.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+
+	public float maxSpeed = 5;
+	public float acceleration = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		int walkDirection = 0;
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			walkDirection = -1;
+				}
+		if (Input.GetKey (KeyCode.D)) {
+			
+			walkDirection += 1;
+				}
+
+		if (walkDirection > 0 && rigidbody2D.velocity.x < maxSpeed) {
+						rigidbody2D.AddForce (Vector2.right * acceleration);
+				} else if (walkDirection < 0 && rigidbody2D.velocity.x > -maxSpeed) {
+			rigidbody2D.AddForce (Vector2.right * -acceleration);
+
+
+				}
+	}
+}
