74fd162f0dbb29306eb212b9207c596be45886fb
Jan Hackl <jan@brokenrul.es>
2015-01-14 19:23:55 +0100

    added pixel manipulation test

--- ComboPainter/Assets/Canvas.cs
+++ ComboPainter/Assets/Canvas.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Canvas : MonoBehaviour {
 
+	public Vector3 position;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,10 @@ public class Canvas : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		position = Input.mousePosition;
+		sprite.sprite.texture.SetPixel((int)position.x, (int)position.y, Color.red);
+		sprite.sprite.texture.Apply();
 	}
 }
