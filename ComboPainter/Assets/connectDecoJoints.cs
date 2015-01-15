c5cbbd1b412acb66a5c6258681a3a3206bbd5726
Peter Vorlaufer <peter@brokenrul.es>
2015-01-15 16:09:31 +0100

    connecting joints to parent main body parts.

---
+++ ComboPainter/Assets/connectDecoJoints.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class connectDecoJoints : MonoBehaviour {
+
+	public string parentName;
+
+	// Use this for initialization
+	void Start () {
+		HingeJoint2D[] joints = gameObject.GetComponents<HingeJoint2D> ();
+		foreach (HingeJoint2D j in joints)
+			if (j.connectedBody == null) 
+			{
+				j.connectedBody = GameObject.Find(parentName).GetComponent<Rigidbody2D>();
+				j.connectedAnchor = j.connectedBody.gameObject.transform.InverseTransformPoint(gameObject.transform.TransformPoint(j.anchor));
+			}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
