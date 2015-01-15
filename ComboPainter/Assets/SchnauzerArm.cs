377352b4ea736267fc01dc80a22605b9c35e2272
Peter Vorlaufer <peter@brokenrul.es>
2015-01-15 17:53:25 +0100

    chnauzer and king audio fix

---
+++ ComboPainter/Assets/SchnauzerArm.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SchnauzerArm : MonoBehaviour {
+
+	public float speed = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HingeJoint2D j = gameObject.GetComponent<HingeJoint2D> ();
+		JointMotor2D m = j.motor;
+		if (Input.GetKey ("up"))
+			m.motorSpeed = speed;
+		else if (Input.GetKey ("down"))
+			m.motorSpeed = -speed;
+		else
+			m.motorSpeed = 0;
+
+		j.motor = m;
+	}
+}
