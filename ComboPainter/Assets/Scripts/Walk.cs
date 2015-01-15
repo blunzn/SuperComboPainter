7eb6cf86469ef4fc6efa99b6529147763ca4e764
Felix Bohatsch <felix@brokenrul.es>
2015-01-15 16:09:22 +0100

    no flipping for unanimated legs

--- ComboPainter/Assets/Scripts/Walk.cs
+++ ComboPainter/Assets/Scripts/Walk.cs
@@ -64,7 +64,7 @@ public class Walk : MonoBehaviour {
 			walk.transform.localScale = scale;
 			idle.transform.localScale = scale;
 		}
-		else
-			transform.localScale = scale;
+//		else
+//			transform.localScale = scale;
 	}
 }
