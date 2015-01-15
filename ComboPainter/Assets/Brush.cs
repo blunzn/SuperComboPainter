eb9067c709e8ce235bb836f189a6dc04bec07b8b
Jan Hackl <jan@brokenrul.es>
2015-01-15 12:15:20 +0100

    color get/set function that recalculates the brush

--- ComboPainter/Assets/Brush.cs
+++ ComboPainter/Assets/Brush.cs
@@ -5,7 +5,7 @@ public class Brush : MonoBehaviour {
 
 	public int width = 21;
 	public int height = 21;
-	public Color color = Color.red;
+	public Color brushColor = Color.red;
 
 	public Texture2D texture;
 
@@ -16,40 +16,50 @@ public class Brush : MonoBehaviour {
 		get { return brush; }
 	}
 
+	public Color color {
+		get { return brushColor; }
+		set {
+			brushColor = value;
+
+			if (texture != null)
+			{
+				brush = texture.GetPixels();
+				width = texture.width;
+				height = texture.height;
+				
+				for (int i = 0; i < brush.Length; i++)
+				{
+					brush[i] *= brushColor;
+				}
+			}
+			else
+			{
+				brush = new Color[width * height];
+				Vector2 centerOffset;
+				for (int x = 0; x < width; x++)
+				{
+					for (int y = 0; y < height; y++)
+					{
+						int index = x + y*width;
+						centerOffset.x = (width/2 + 1) - x;
+						centerOffset.y = (height/2 + 1) - y;
+						
+						Color color = brushColor;
+						color.a = (centerOffset.magnitude < width/2) ? 1 : 0;
+						brush[index] = color;
+					}
+				}
+			}
+
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 
 		canvas = GameObject.Find("Canvas").GetComponent<PaintArea>();
+		color = brushColor;
 
-		if (texture != null)
-		{
-			brush = texture.GetPixels();
-			width = texture.width;
-			height = texture.height;
-
-			for (int i = 0; i < brush.Length; i++)
-			{
-				brush[i] *= color;
-			}
-		}
-		else
-		{
-			brush = new Color[width * height];
-			Vector2 centerOffset;
-			for (int x = 0; x < width; x++)
-			{
-				for (int y = 0; y < height; y++)
-				{
-					int index = x + y*width;
-					centerOffset.x = (width/2 + 1) - x;
-					centerOffset.y = (height/2 + 1) - y;
-
-					Color brushColor = color;
-					brushColor.a = (centerOffset.magnitude < width/2) ? 1 : 0;
-					brush[index] = brushColor;
-				}
-			}
-		}
 	}
 	
 	// Update is called once per frame
