b612bc8fea38153e7010dd938db619bac646e1e7
Peter Vorlaufer <peter@brokenrul.es>
2015-01-15 13:02:36 +0100

    screaming head works (use scream script in top)

---
+++ ComboPainter/Assets/Standard Assets/Scripts/Camera Scripts/Scream.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Scream : MonoBehaviour {
+	
+	private SpriteRenderer headIdle;
+	private SpriteRenderer headScream;
+	
+	// Use this for initialization
+	void Start () {
+		Transform head = gameObject.transform.FindChild ("head");
+		headIdle = head.FindChild ("idle").gameObject.GetComponent<SpriteRenderer> ();
+		headScream = head.FindChild ("scream").gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool doScream = Input.GetAxis ("green") > 0 || Input.GetAxis ("blue") > 0 || Input.GetAxis ("red") > 0;
+		headIdle.enabled = !doScream;
+		headScream.enabled = doScream;
+	}
+}
