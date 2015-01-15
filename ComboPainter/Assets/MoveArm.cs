94eef7dff6a2925c911d693cef75d1b68ab00b5a
Peter Vorlaufer <peter@brokenrul.es>
2015-01-15 10:48:16 +0100

    removed left arm, arm movement with up/down

--- ComboPainter/Assets/MoveArm.cs
+++ ComboPainter/Assets/MoveArm.cs
@@ -3,25 +3,32 @@ using System.Collections;
 
 public class MoveArm : MonoBehaviour {
 	public float speed = 500;
-	public string key = "a";
+	public string up = "up";
+	public string down = "down";
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		JointMotor2D motor = gameObject.GetComponent<SliderJoint2D> ().motor;
-		SliderJoint2D slider = gameObject.GetComponent<SliderJoint2D> ();
-		if (Input.GetKey (key)) {
-			slider.useMotor = true;
+	void Update () 
+	{
+		SliderJoint2D slider = gameObject.GetComponent<SliderJoint2D>();
+		JointMotor2D motor = slider.motor;
+		if (Input.GetKey(up))
+			motor.motorSpeed = -speed;
+		else if (Input.GetKey(down))
+			motor.motorSpeed = speed;
+		else
+			motor.motorSpeed = 0;
+			//slider.useMotor = true;
 //				} else if (Input.GetKey (KeyCode.DownArrow)) {
 //			
 //						motor.motorSpeed = speed;
-				} else {
-			slider.useMotor = false;
-				}
+//				} else {
+//			slider.useMotor = false;
+//				}
 
-//		gameObject.GetComponent<SliderJoint2D> ().motor = motor;
+		slider.motor = motor;
 	}
 }
