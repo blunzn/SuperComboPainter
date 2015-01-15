5e926dbf1f7aa2566da0aeb451d05ff1e6c1a30a
Felix Bohatsch <felix@brokenrul.es>
2015-01-15 15:35:23 +0100

    walking will flip characters

--- ComboPainter/Assets/Scripts/Walk.cs
+++ ComboPainter/Assets/Scripts/Walk.cs
@@ -32,14 +32,22 @@ public class Walk : MonoBehaviour {
 	void Update () {
 	
 		float walkDirection = Input.GetAxis("walk");
+		
+		Vector3 scale = Vector3.one;
 
-		if (walkDirection > 0 && rigidbody2D.velocity.x < maxSpeed) {
-						rigidbody2D.AddForce (Vector2.right * acceleration);
-				} else if (walkDirection < 0 && rigidbody2D.velocity.x > -maxSpeed) {
-			rigidbody2D.AddForce (Vector2.right * -acceleration);
+		if (walkDirection > 0) {
+			scale.x = 1;
+			if(rigidbody2D.velocity.x < maxSpeed)
+				rigidbody2D.AddForce (Vector2.right * acceleration);
 
+		} else if (walkDirection < 0) 
+		{
+			scale.x = -1;
+			if(rigidbody2D.velocity.x > -maxSpeed)
+				rigidbody2D.AddForce (Vector2.right * -acceleration);
+		}
 
-				}
+		transform.localScale = scale;
 
 		if (hasAnimations) {
 			if (rigidbody2D.velocity.magnitude > 0.5) {
