f599c977f3ab66d6bd30546a7fa9ccf7439481fb
Felix Bohatsch <felix@brokenrul.es>
2015-01-16 14:19:25 +0100

    fixed finehide

--- ComboPainter/Assets/Scripts/CharacterManager.cs
+++ ComboPainter/Assets/Scripts/CharacterManager.cs
@@ -58,10 +58,10 @@ public class CharacterManager : MonoBehaviour {
 		if (Input.GetKeyDown ("n"))
 						newCrewSet ();
 
-		if (Input.GetButtonDown("increaseFinehide") && finehide < 2)
+		if (Input.GetButtonDown("decreaseFinehide") && finehide < 2)
 			finehide++;
 				
-		if (Input.GetButtonDown("decreaseFinehide") && finehide > 0)
+		if (Input.GetButtonDown("increaseFinehide") && finehide > 0)
 			finehide--;
 	}
 	
