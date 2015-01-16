d876bc770c07afea5d551ea9a14111da8c4f2e76
Felix Bohatsch <felix@brokenrul.es>
2015-01-16 11:29:54 +0100

    added but a & nike

---
+++ ComboPainter/Assets/ContractGummi.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContractGummi : MonoBehaviour {
+	public float frequencyIncrease = 0.01f;
+
+	private CharacterManager manager;
+	private SpringJoint2D joint;
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("GameController").GetComponent<CharacterManager> ();
+		joint = GetComponent<SpringJoint2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("moveArm") > 0) {
+						joint.frequency = Mathf.Min (joint.frequency + frequencyIncrease, 10);
+				} else
+						joint.frequency = 0.001f;
+	}
+}
