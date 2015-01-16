b8fdb25e348f12f8459d2e63fa905d4122d4e1c7
Felix Bohatsch <felix@brokenrul.es>
2015-01-16 10:52:03 +0100

    added dot

---
+++ ComboPainter/Assets/RotateBall.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBall : MonoBehaviour {
+
+	public float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float walkDirection = Input.GetAxis("walk");
+		
+		Vector3 rotation = transform.rotation.eulerAngles;
+		
+		CharacterManager manager = GameObject.Find("GameController").GetComponent<CharacterManager> ();
+		
+		if (walkDirection > 0) {
+			rotation.z -= speed;
+//			if(rigidbody2D.velocity.x < maxSpeed/(float)(manager.finehide+1))
+//				rigidbody2D.AddForce (Vector2.right * acceleration);
+			
+		} else if (walkDirection < 0) 
+		{
+			rotation.z -= speed;
+			//			if(rigidbody2D.velocity.x > -maxSpeed/(float)(manager.finehide+1))
+//				rigidbody2D.AddForce (Vector2.right * -acceleration);
+		}
+
+		Quaternion rotQuat = Quaternion.Euler (rotation);
+
+		transform.rotation = rotQuat;
+
+	}
+}
